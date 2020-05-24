15aea5bd7627657f8fc22779da66b29b787e6d63
Grandmagic13 <jabencefear@hotmail.com>
2020-05-24 15:53:21 +0200

    Add: Created prototype of Fill fields step #3

--- test_automation_task/exercises.py
+++ test_automation_task/exercises.py
@@ -1,10 +1,14 @@
 import time
 import unittest
 
+from hamcrest import assert_that
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from test_automation_task.constants import *
+from test_automation_task.custom_matchers.wait_for_matcher import waits_to_have
 from test_automation_task.page_objects.featured_file_filtering_page import FeaturedFileFilteringPage
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class Exercises(unittest.TestCase):
@@ -29,6 +33,18 @@ class Exercises(unittest.TestCase):
         page.set_age_slider_to(40)
 
         # 3.	Sort order by „Life Span”
+
+        item_picker_locator = (By.XPATH, "//label[text()='Sort by']/following::span[@class='comboBoxItemPicker']")
+        life_span_option_locator = (By.XPATH, "//div[text()='Life Span']")
+
+        assert_that(self, waits_to_have(ec.element_to_be_clickable, item_picker_locator))
+        self.driver.find_element(*item_picker_locator).click()
+
+        assert_that(self, waits_to_have(ec.element_to_be_clickable, life_span_option_locator))
+        self.driver.find_element(*life_span_option_locator).click()
+
+
+
         # 4.	Select checkbox  to Ascending
         #
         # Assert that results contains more than 12 items
