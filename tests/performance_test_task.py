03657b26b589db6dbdc88756ee86cc72f7a4e4f0
Grandmagic13 <jabencefear@hotmail.com>
2020-05-27 02:30:34 +0200

    Modify: Expanded performance test case for 10 simultaneous threads
    - Assertion checks response times returned by each thread

--- tests/performance_test_task.py
+++ tests/performance_test_task.py
@@ -1,7 +1,8 @@
-import time
+import concurrent.futures
 import unittest
+from concurrent.futures import as_completed
 
-from hamcrest import assert_that, less_than_or_equal_to
+from hamcrest import assert_that, less_than_or_equal_to, only_contains
 from selenium import webdriver
 
 from tests.constants import DEFAULT_WAIT_TIME
@@ -10,10 +11,16 @@ from tests.page_objects.amazon import AmazonPage
 
 class PerformanceTest(unittest.TestCase):
     def test_search_performance(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(DEFAULT_WAIT_TIME)
-        page = AmazonPage(self.driver)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+            number_of_threads = 10
+            futures = [executor.submit(self.measure_search) for i in range(number_of_threads)]
+            response_times = [completed.result() for completed in as_completed(futures)]
+        assert_that(response_times, only_contains(less_than_or_equal_to(1000)))
+
+    def measure_search(self):
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(DEFAULT_WAIT_TIME)
+        page = AmazonPage(driver)
         page.go_to_page_url()
         page.search_for_item("Playstation 4")
-        response_time = page.measure_response_time_in_milliseconds()
-        assert_that(response_time, less_than_or_equal_to(1000))
+        return page.measure_response_time_in_milliseconds()
