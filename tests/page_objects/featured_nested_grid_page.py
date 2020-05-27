d283ecd1fff8c076ec3c513c186739cdfdeb79ea
Grandmagic13 <jabencefear@hotmail.com>
2020-05-27 07:04:54 +0200

    Add: Exercise 3 work in progress #2
    - Open first item regardless of name containing Correction or not
    - Modify all subitem descriptions

--- tests/page_objects/featured_nested_grid_page.py
+++ tests/page_objects/featured_nested_grid_page.py
@@ -27,25 +27,38 @@ class FeaturedNestedGridPage(SmartClientPage):
         self.url = create_subpage_url(self.url, "#featured_nested_grid")
 
     def change_sub_items_for_items_with_name_containing_correction(self):
+        # check if row / item needs to be modified
+
+        # if needs to be modified
         # change 1 item without conditions
         arrow_1_locator = self.arrow_locator_by_index(1)
         self.click(arrow_1_locator)
-        #modify elements if any
-        description_1_locator = self.nested_item_description_locator_by_index(1)
-        self.click(description_1_locator)
-        new_description = "{index} {random_string}".format(index=1, random_string=self.get_random_10_long_string())
-        self.send_keys_to_element(self.TEXTAREA_LOCATOR, new_description)
-        time.sleep(2)
-        #
-        self.click(self.SAVE_BUTTON_LOCATOR)
-        time.sleep(2)
+        nested_index = 1
+        while self.description_by_index_exists(nested_index):
+            description_locator = self.description_locator_by_index(nested_index)
+            self.move_to_element(description_locator)
+            self.click(description_locator)
+            new_description = "{index} {random_string}".format(index=nested_index, random_string=self.get_random_10_long_string())
+            self.send_keys_to_element(self.TEXTAREA_LOCATOR, new_description)
+            self.click(self.SAVE_BUTTON_LOCATOR)
+            nested_index += 1
+
         self.click(self.CLOSE_BUTTON_LOCATOR)
+
+    def description_by_index_exists(self, index):
+        return len(self.find_elements(self.description_locator_by_index(index))) > 0
+
+    def move_to_element(self, locator):
+        element = self.find_element(locator)
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element)
+        actions.perform()
 
     def arrow_locator_by_index(self, index):
         return By.XPATH, self.ARROW_BY_INDEX_XPATH.format(index=index)
 
 
-    def nested_item_description_locator_by_index(self, index):
+    def description_locator_by_index(self, index):
         return By.XPATH, self.NESTED_ITEM_DESCRIPTION_BY_INDEX_XPATH.format(index=index)
 
     def get_random_10_long_string(self):
