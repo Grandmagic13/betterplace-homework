a871e4f0b20a0b10cb0f2f3f5b22858e690c853b
Grandmagic13 <jabencefear@hotmail.com>
2020-05-27 07:43:54 +0200

    Add: Exercise 3 work in progress #3
    - Opens items if name containing Correction but stops at unloaded items so far
    - Modify all subitem descriptions

--- tests/page_objects/featured_nested_grid_page.py
+++ tests/page_objects/featured_nested_grid_page.py
@@ -14,6 +14,7 @@ class FeaturedNestedGridPage(SmartClientPage):
     FOLLOWING_ITEM_XPATH = "//div[text()='Item']/following::"
     FOLLOWING_DESCRIPTION_XPATH = "//div[text()='Description']/following::"
     ITEM_BY_INDEX_XPATH = FOLLOWING_ITEM_XPATH + "tr[@role='listitem' and @aria-posinset='{index}']"
+    ITEM_NAME_BY_INDEX_XPATH = ITEM_BY_INDEX_XPATH + "//td[2]/div"
     NESTED_ITEM_BY_INDEX_XPATH = FOLLOWING_DESCRIPTION_XPATH + "tr[@role='listitem' and @aria-posinset='{index}']"
     NESTED_ITEM_DESCRIPTION_BY_INDEX_XPATH = NESTED_ITEM_BY_INDEX_XPATH + "//td[3]/div"
     ARROW_BY_INDEX_XPATH = ITEM_BY_INDEX_XPATH + "//span"
@@ -27,35 +28,68 @@ class FeaturedNestedGridPage(SmartClientPage):
         self.url = create_subpage_url(self.url, "#featured_nested_grid")
 
     def change_sub_items_for_items_with_name_containing_correction(self):
-        # check if row / item needs to be modified
+        index = 1
 
-        # if needs to be modified
-        # change 1 item without conditions
-        arrow_1_locator = self.arrow_locator_by_index(1)
-        self.click(arrow_1_locator)
-        nested_index = 1
-        while self.description_by_index_exists(nested_index):
-            description_locator = self.description_locator_by_index(nested_index)
-            self.move_to_element(description_locator)
-            self.click(description_locator)
-            new_description = "{index} {random_string}".format(index=nested_index, random_string=self.get_random_10_long_string())
-            self.send_keys_to_element(self.TEXTAREA_LOCATOR, new_description)
-            self.click(self.SAVE_BUTTON_LOCATOR)
-            nested_index += 1
+        # tricky, only moves up to dynamically loaded elements, need to move slider to load more?
+        while self.item_by_index_exists(index):
+            item_locator = self.item_locator_by_index(index)
+            self.move_to_element(item_locator)
 
-        self.click(self.CLOSE_BUTTON_LOCATOR)
+            if self.item_by_index_text_contains(index, "Correction"):
+                arrow_locator = self.arrow_locator_by_index(index)
+                self.click(arrow_locator)
+                nested_index = 1
+                while self.description_by_index_exists(nested_index):
+                    description_locator = self.description_locator_by_index(nested_index)
+                    self.move_to_element_and_click(description_locator)
 
-    def description_by_index_exists(self, index):
-        return len(self.find_elements(self.description_locator_by_index(index))) > 0
+                    new_description = "{index} {random_string}".format(index=nested_index, random_string=self.get_random_10_long_string())
+                    self.send_keys_to_element(self.TEXTAREA_LOCATOR, new_description)
+                    self.move_to_element_and_click(self.SAVE_BUTTON_LOCATOR)
+                    nested_index += 1
+
+                self.move_to_element_and_click(self.CLOSE_BUTTON_LOCATOR)
+            index += 1
+
+            # quick last check if there are more elements not yet dynamically loaded?
+            if not self.item_by_index_exists(index):
+                self.move_to_element(item_locator)
+                # get slider location and move it there?
+
+    def move_to_element_and_click(self, locator):
+        self.move_to_element_and_optionally_click(locator, True)
 
     def move_to_element(self, locator):
+        self.move_to_element_and_optionally_click(locator, False)
+
+    def move_to_element_and_optionally_click(self, locator, click):
         element = self.find_element(locator)
         actions = ActionChains(self.driver)
         actions.move_to_element(element)
+        if click:
+            actions.click()
         actions.perform()
+
+    def item_by_index_text_contains(self, index, substring):
+        return substring in self.find_element(self.item_name_locator_by_index(index)).text
+
+    def item_name_locator_by_index(self, index):
+        return By.XPATH, self.ITEM_NAME_BY_INDEX_XPATH.format(index=index)
+
+    def item_locator_by_index(self, index):
+        return By.XPATH, self.ITEM_BY_INDEX_XPATH.format(index=index)
 
     def arrow_locator_by_index(self, index):
         return By.XPATH, self.ARROW_BY_INDEX_XPATH.format(index=index)
+
+    def description_by_index_exists(self, index):
+        return self.element_exists(self.description_locator_by_index(index))
+
+    def item_by_index_exists(self, index):
+        return self.element_exists(self.item_locator_by_index(index))
+
+    def element_exists(self, locator):
+        return len(self.find_elements(locator)) > 0
 
 
     def description_locator_by_index(self, index):
